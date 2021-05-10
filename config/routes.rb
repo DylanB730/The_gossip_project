10d5c89040996a83d023b479b8bdd287ce2be3a1
DylanB730 <dylan.bonafos@gmail.com>
2021-05-11 01:16:43 +0200

    Projet validant THP gossip

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'pages#contact'
 
+  get '/welcome/:name', to: 'pages#welcome'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
